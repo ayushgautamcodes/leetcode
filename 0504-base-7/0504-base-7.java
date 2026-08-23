5d0b8cb25587f3e12afb9b0c6742be3fb7e5df2f
Ayush Pratap Gautam <iayushgautam7@gmail.com>
2026-08-23 15:50:24 +0530

    Time: 4 ms (13.89%), Space: 42.3 MB (98.96%) - LeetHub

--- 0504-base-7/0504-base-7.java
+++ 0504-base-7/0504-base-7.java
@@ -1,12 +1,16 @@
 class Solution {
     public String convertToBase7(int num) {
         long ans = 0;
-        long pv = 1;
-        while(num!=0){
-            long rem = num%7;
-            ans = ans+(rem*pv);
-            pv*=10;
-            num/=7;
+        if(num==0){
+            ans = 0;
+        }else{
+            long pv = 1;
+            while(num!=0){
+                long rem = num%7;
+                ans = ans+(rem*pv);
+                pv*=10;
+                num/=7;
+            }
         }
         return (num<0 ? "-":"")+ans;
     }
