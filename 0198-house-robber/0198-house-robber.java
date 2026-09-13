38bd614951b9cd61f698770d5f42175f57c04e79
Ayush Pratap Gautam <iayushgautam7@gmail.com>
2026-09-13 19:22:02 +0530

    Time: 0 ms (100%), Space: 42.8 MB (56.55%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int rob(int[] nums) {
+        int prevMax = 0; 
+        int currMax = 0;
+    
+        for(int num : nums) {
+            int temp = currMax;
+        
+            currMax = Math.max(currMax, prevMax + num);
+            prevMax = temp;
+        }
+        return currMax;
+    }
+}
